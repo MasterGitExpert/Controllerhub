b3065ff014f497da9c04151ccb975c7b6afe4c5c
ayden8383 <ayden_tranle@hotmail.com>
2025-09-30 12:43:00 +1000

    Configured Account Routing and functionality for non logged in user

--- ecommerce/storefront/urls.py
+++ ecommerce/storefront/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('storefront.urls')),
-    path("account/", views.account, name="account"),
+    path('', views.home, name='home'),       # homepage
+    path('account/', views.account, name='account'),  # account page
 ]
