74392521796b628dbfb4e3aa54748b8d2f75a11e
Krish <krish.patel-1@student.uts.edu.au>
2025-09-30 20:03:05 +1000

    Image not showing bug fix

--- ecommerce/storefront/models.py
+++ ecommerce/storefront/models.py
@@ -1,4 +1,5 @@
 """Django data models for the ecommerce storefront application."""
+from tabnanny import verbose
 from django.db import models
 import datetime
 
@@ -24,6 +25,39 @@ class Customer(models.Model):
         return f"{self.last_name} {self.first_name} - {self.email}"
 
 
+class ProductCategory(models.Model):
+    """
+        ProductCategory table to store product category information like 
+        chairs, keyboards, mouse etc.
+
+        - name
+    """
+    class Meta:
+        verbose_name = "Product Category"
+        verbose_name_plural = "Product Categories"
+
+    name = models.CharField(max_length=16)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class ProductRange(models.Model):
+    """
+        ProductRange table to store product range information like homebrand,
+        premium, limited edition etc.
+        - name
+    """
+    class Meta:
+        verbose_name = "Product Range"
+        verbose_name_plural = "Product Ranges"
+
+    name = models.CharField(max_length=16)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
 class Product(models.Model):
     """
         Product table to store product information
@@ -35,14 +69,33 @@ class Product(models.Model):
         - display_item
     """
     name = models.CharField(max_length=128)
+    tagline = models.CharField(max_length=64, default="", blank=True)
     description = models.CharField(max_length=256, default="No description")
     price = models.DecimalField(max_digits=8, decimal_places=2, default=5.00)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='uploads/products/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='uploads/products/', blank=True, null=True)
     display_item = models.BooleanField(default=True)
+    # Product category and range (for filtering)
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.CASCADE, null=True)
+    range = models.ForeignKey(
+        ProductRange, on_delete=models.CASCADE, null=True)
+    # Sale information for discounted items
+    discount = models.BooleanField(default=False)
+    sale_price = models.DecimalField(
+        max_digits=8, decimal_places=2, default=3.98)
 
     def __str__(self) -> str:
-        return f"{self.name} - {self.stock} @ ${self.price}"
+        if self.discount:
+            striked_price = "".join(
+                [f'{char}\u0336' for char in f"${self.price}"])
+            price = f"{striked_price} -> ${self.sale_price}"
+        else:
+            price = f"${self.price}"
+        return f"{self.name} - {self.stock} @ {price} " \
+            f"[{self.category}, {self.range}]"
+
 
 class Order(models.Model):
     """
@@ -74,5 +127,3 @@ class OrderItem(models.Model):
 
     def __str__(self) -> str:
         return f"Order Item {self.order.order_id}: {self.product.name} x {self.quantity} = ${self.product.price * self.quantity}"
-
-
