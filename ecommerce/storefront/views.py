ec1091931b9b4d24176271f5d3956f63621ce01a
ayden8383 <ayden_tranle@hotmail.com>
2025-09-30 12:29:02 +1000

    added account Functionality

--- ecommerce/storefront/views.py
+++ ecommerce/storefront/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     products = Product.objects.all().order_by('name')
     return render(request, "index.html", {'products': products})
+
+@login_required
+def account(request):
+    return render(request, "account.html")
