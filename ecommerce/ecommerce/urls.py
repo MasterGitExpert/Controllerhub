56fb4925f6f2306c486eb3fece42b1051feb2daf
liyose <leoluk2211@gmail.com>
2025-10-01 12:22:53 +1000

    Working cart page, broken checkout page
    Cart page is done, the checkout page isnt working, meaning no item is being added or updated in the database so far. that occurs in the checkout page.

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('storefront.urls')),
+    path('', include('storefront.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
