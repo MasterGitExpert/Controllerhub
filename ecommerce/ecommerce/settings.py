33f61e50da31182747ca309f36668557e9bc5527
Krish <krish.patel-1@student.uts.edu.au>
2025-10-23 09:32:57 +1100

    webserver deploy static file fix

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "20.211.64.15",
                  "wavelengthcontrollerhub.azurewebsites.net", os.environ.get('WEBSITE_HOSTNAME')]
